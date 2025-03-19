ec3cec5e5f804a53cae0a0b38d87f5f78b5ff5c5
d.lyanguzov <d.lyanguzov@geoscan.ru>
2025-03-19 23:09:17 +0300

    Task 1 done with unit tests

--- lab1/src/main/kotlin/task1/TaylorSeries.kt
+++ lab1/src/main/kotlin/task1/TaylorSeries.kt
@@ -1,18 +1,29 @@
 package task1
 
+import kotlin.math.abs
 import kotlin.math.pow
 
-fun sin(x: Double, terms: Int): Double {
+fun sinTaylorSeries(x: Double, terms: Int): Double {
+    val resultSign = if (x >= 0) 1.0 else -1.0
+    val normalizedX = abs(x) % (2 * Math.PI)
+
+    val adjustedX = when {
+        (normalizedX > Math.PI / 2) and (normalizedX <= Math.PI) -> Math.PI - normalizedX
+        (normalizedX > Math.PI) and (normalizedX <= 3 * Math.PI / 2) -> Math.PI - normalizedX
+        (normalizedX > 3 * Math.PI / 2) and (normalizedX <= 2 * Math.PI) -> normalizedX - 2 * Math.PI
+        else -> normalizedX
+    }
+
     var result = 0.0
     var sign = 1.0
 
     for (n in 0 until terms) {
-        val term = sign * (x.pow((2 * n + 1).toDouble()) / factorial(2 * n + 1))
+        val term = sign * (adjustedX.pow((2 * n + 1).toDouble()) / factorial(2 * n + 1))
         result += term
         sign *= -1
     }
 
-    return result
+    return result * resultSign
 }
 
 fun factorial(n: Int, accumulator: Int = 1): Int = when {
--- lab1/src/test/kotlin/TaylorSeriesTest.kt
+++ lab1/src/test/kotlin/TaylorSeriesTest.kt
@@ -3,9 +3,15 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import task1.factorial
+import task1.sinTaylorSeries
 
 
 class TaylorSeriesTest {
+    companion object {
+        val ACCURACY = 1e-3
+    }
+
+
     @ParameterizedTest
     @CsvSource(
         "0, 1",
@@ -30,5 +36,22 @@ class TaylorSeriesTest {
         assertThrows(IllegalArgumentException::class.java) {
             factorial(input)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0.0, 0.0",
+        "0.5235987755982988, 0.5", // Math.PI / 6
+        "0.7853981633974483, 0.7071067811865475", // Math.PI / 4
+        "1.0471975511965976, 0.8660254037844387", // Math.PI / 3
+        "1.5707963267948966, 1.0", // Math.PI / 2
+        "3.141592653589793, 0.0", // Math.PI
+        "4.71238898038469, -1.0", // 3 * Math.PI / 2
+        "6.283185307179586, 0.0" // 2 * Math.PI
+    )
+    fun `sin taylor series periodic test`(input: Double, expected: Double) {
+        val terms = 9 // Количество членов ряда
+        val result = sinTaylorSeries(input, terms)
+        assertEquals(expected, result, ACCURACY) // Используем допустимую погрешность
     }
 }
